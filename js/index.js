39100d4ddeca29f338bc04183605584be0cfbfc2
vodacluc15@gmail.com <vodacluc@Vo-Dac-Luc.local>
2023-10-07 13:05:37 +0700

    aaa

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function toggleHelp() {
+    const subactionElement = document.querySelector('.header-actions .sub-action');
+    console.log(subactionElement);
+    subactionElement.classList.toggle('active');
+}
